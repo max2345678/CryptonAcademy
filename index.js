d436bcd1a4610077a4f023f5ac7cbb7e0843aba2
max2345678 <102423290+max2345678@users.noreply.github.com>
2022-03-26 16:40:33 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+echo "# CryptonAcademy" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/maks235355/CryptonAcademy.git
+git push -u origin main
